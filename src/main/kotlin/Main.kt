fdc7d6500e49ee03a65ec32380a2a60d949f458f
Apachi144 <yessinejawa@gmail.com>
2022-09-11 19:58:35 +0100

    adding some notes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,13 +3,23 @@ val varGlobal : String? = null
 fun main(){
     println("Hello World")
 
-    // for variables, we can use
-    // val : only read ones  , but var : readable and reassigned
-
-    // types in kotlin are non nullables by default
-    // we should do String? for example
     val nameOnlyRead : String = "yessine"
     var nameReassigned : String = "jaoua"
 
+    when (varGlobal){
+        null -> println("hahaha")
+        else -> println("lololo")
+    }
+
+    val greetingToPrint = when(nameReassigned){
+        null -> "Hi"
+        else -> "Hello"
+    }
+
+    print("this is the message$greetingToPrint")
+
+    fun haha() : String{
+        return "yessine"
+    }
     println(nameReassigned)
 }
