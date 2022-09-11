b49d88d074692b99248eb2efcda5c6879ddb4318
Apachi144 <yessinejawa@gmail.com>
2022-09-11 19:17:14 +0100

    adding first files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
+val varGlobal : String? = null
+
 fun main(){
     println("Hello World")
+
+    // for variables, we can use
+    // val : only read ones  , but var : readable and reassigned
+
+    // types in kotlin are non nullables by default
+    // we should do String? for example
+    val nameOnlyRead : String = "yessine"
+    var nameReassigned : String = "jaoua"
+
+    println(nameReassigned)
 }
