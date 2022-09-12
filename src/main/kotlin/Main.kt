6d3676efbf0df5178cfb1c8e96f58a19ea73a633
Apachi144 <yessinejawa@gmail.com>
2022-09-12 10:59:28 +0100

    adding the function file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,43 @@
-val varGlobal : String? = null
+val varGlobal: String? = null
 
-fun main(){
+fun main() {
     println("Hello World")
 
-    val nameOnlyRead : String = "yessine"
-    var nameReassigned : String = "jaoua"
+    val nameOnlyRead: String = "yessine"
+    var nameReassigned: String = "jaoua"
 
-    when (varGlobal){
+    when (varGlobal) {
         null -> println("hahaha")
         else -> println("lololo")
     }
 
-    val greetingToPrint = when(nameReassigned){
+    val greetingToPrint = when (nameReassigned) {
         null -> "Hi"
         else -> "Hello"
     }
 
     print("this is the message$greetingToPrint")
 
-    fun haha() : String{
+    fun haha(): String {
         return "yessine"
     }
+
+    val newArray = arrayOf("Hahaha", "Hahah2")
+    println(newArray[0])
+
+    newArray.forEach { println(it) }
+
+    val map = mapOf(1 to "a" , 2 to "b" , 3 to "c")
+
+    map.forEach{ (k, v) -> println(" $k  sldkjfsdf $v") }
+
+    val map3 = mutableMapOf(1 to "a"  , 2 to "b")
+
+    map3[5] = "d"
+
+    println("this is the value of map3 $map3")
+
     println(nameReassigned)
 }
+
+
